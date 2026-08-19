894abe6d13481d9b8a536f8d83ffb2c87080c4fe
Sithum2002 <sithumasam@gmail.com>
2026-08-19 17:37:18 +0530

    feat(config): add Spring Boot application entry point

---
+++ src/main/java/com/sunrise/dental/DentalApplication.java
@@ -0,0 +1,18 @@
+package com.sunrise.dental;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
+import org.springframework.scheduling.annotation.EnableAsync;
+import org.springframework.scheduling.annotation.EnableScheduling;
+
+@SpringBootApplication
+@EnableJpaAuditing(auditorAwareRef = "auditorAware")
+@EnableScheduling
+@EnableAsync
+public class DentalApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DentalApplication.class, args);
+    }
+}
